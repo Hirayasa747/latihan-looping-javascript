89ca48affd72f7fd60d00f8ad518ec78e0cb4866
Hirayasa747 <hirayasa7@gmail.com>
2026-06-24 14:06:19 +0700

    up perbaikan

--- soal18.js
+++ soal18.js
@@ -4,18 +4,15 @@ let n = parseInt(prompt("Masukkan jumlah perkalian:"));
 let hasil = "\t";
 
 for (let i = 1; i <= n; i++) {
-  hasil += i + "\t";
-}
-hasil += "\n";
+  hasil += i + "\t";}
+  hasil += "\n";
 
 for (let i = 1; i <= n; i++) {
   hasil += i + "\t"; 
+  
+for (let j = 1; j <= n; j++) {
+    hasil += (i * j) + "\t";}
 
-  for (let j = 1; j <= n; j++) {
-    hasil += (i * j) + "\t";
-  }
-
-  hasil += "\n";
-}
+  hasil += "\n";}
 
 console.log(hasil);
